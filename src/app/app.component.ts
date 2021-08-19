3d0701e253a30d91c900fa652997877325f1384c
احمد علاء الدين <UGS.140279@ci.suez.ed>
2021-08-19 18:06:24 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users :any;
+
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit() {
+   this.http.get("https://localhost:44390/api/users").subscribe(x =>
+    {
+      this.users = x;
+      console.log(this.users);
+
+    });
+  }
+}
